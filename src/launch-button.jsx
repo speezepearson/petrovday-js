ea39b715b42071fff0b4787efa2d9eddf0f881b5
Spencer Pearson <suspense@cs.washington.edu>
2017-09-09 12:07:45 -0700

    fix className of LaunchButton: unable to interpolate in JSX

--- src/launch-button.jsx
+++ src/launch-button.jsx
@@ -17,8 +17,7 @@ class LaunchButton extends React.Component {
     if (typeof this.state.state === 'number') {
       return <button className="launch-button launch-button--ticking"><Countdown time={this.state.state}/></button>;
     }
-    console.log('rendering', this, 'hi');
-    return <button className="launch-button launch-button--{this.state}" onClick={(e) => this.launch()} disabled={this.state.state==='obsolete'}> &#9762; </button>
+    return <button className={`launch-button launch-button--${this.state.state}`} onClick={(e) => this.launch()} disabled={this.state.state==='obsolete'}> &#9762; </button>
   }
 
   launch() {
