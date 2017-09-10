e93f023bea86cbdae491d9f812d1d28a92a1f422
Spencer Pearson <suspense@cs.washington.edu>
2017-09-09 22:17:56 -0700

    pretty up authentication

--- src/authenticator.jsx
+++ src/authenticator.jsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import $ from 'jquery';
 
+import Spinner from 'spin';
+
 import './authenticator.css';
 
 class Authenticator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {password: ''}
+    this.state = {phase: 'idle', password: '', failed: false};
     this.passwordField = <input type="password" placeholder="Password" />;
   }
 
@@ -15,16 +17,30 @@ class Authenticator extends React.Component {
   }
 
   render() {
+    var buttonHolderContents = (
+      (this.state.phase === 'idle') ? <button onClick={() => this.authenticate()}>Log In</button> :
+      (this.state.phase === 'checking') ? <span className='authenticator__spinner' ref={(el) => new Spinner({color: '#fff', length: 5, width: 2, radius: 5}).spin(el)}>&nbsp;</span> :
+      '\u2714'
+    )
     return <div className="authenticator">
-      <input type="password" placeholder="Password" onChange={(e) => this.handleChange(e)} />
-      &nbsp; <button onClick={() => this.authenticate()}>Log In</button>
+      <input type="password" placeholder="Password"
+             className={'authenticator__password-field' + (this.state.failed ? ' failed' : '')}
+             onKeyPress={(e) => {if (e.keyCode===13) this.authenticate()}}
+             onChange={(e) => this.handleChange(e)} />
+      &nbsp;
+      <div className='authenticator__button-holder'>
+        {buttonHolderContents}
+      </div>
     </div>;
   }
 
   authenticate(password) {
+    this.setState({'phase': 'checking'});
     $.get('authenticate', {'password': this.state.password}, (data) => {
-      console.log(data);
+      this.setState({'phase': 'complete'});
       this.props.onSuccessfulAuthentication(JSON.parse(data));
+    }).fail(() => {
+      this.setState({'phase': 'idle', failed: true});
     });
   }
 }
