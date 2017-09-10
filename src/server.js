c817516af50f28e955df926e3013688c78ab7af0
Spencer Pearson <suspense@cs.washington.edu>
2017-09-09 17:41:22 -0700

    give server basic server powers with Express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log("This is where I'd put a server, IF I HAD ONE");
+import path from 'path';
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('dist'));
+
+app.get('/static/app.js', (req, res) => {
+  res.sendFile(path.join(process.cwd(), __dirname, 'dist', 'app.js'));
+});
+
+app.get('/:player/', (req, res) => {
+  res.sendFile(path.join(process.cwd(), __dirname, 'index.html'));
+});
+
+app.listen(5000, function () {
+  console.log(`cwd = ${process.cwd()}`);
+  console.log(`__dirname = ${__dirname}`);
+  console.log(`__filename = ${__filename}`);
+  console.log('listening on port 5000');
+});
